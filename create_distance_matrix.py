d0014c747c1a700d81ae4a1429f272fc6e7ae5a0
patbarry29 <79643532+patbarry29@users.noreply.github.com>
2024-11-17 12:41:15 +0100

    change distance matrix to float not int

--- create_distance_matrix.py
+++ create_distance_matrix.py
@@ -4,9 +4,10 @@ import numpy as np
 def create_distance_matrix(problem):
     nodes = list(problem.get_nodes())
     num_nodes = problem.dimension
-    distance_matrix = np.zeros((num_nodes, num_nodes), dtype=int)
+    distance_matrix = np.zeros((num_nodes, num_nodes), dtype=float)
     for i in range(num_nodes):
         for j in range(num_nodes):
             if i != j:
                 distance_matrix[i][j] = problem.get_weight(nodes[i], nodes[j])
+    print(distance_matrix)
     return distance_matrix
